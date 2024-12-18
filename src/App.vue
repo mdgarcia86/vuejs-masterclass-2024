3fcb1f26b845a0a86bc81c8095aefca9fb5f70bc
Manuel Garcia <mdgarcia.calpoly@gmail.com>
2024-12-18 08:28:29 -0800

    chapter 2:13 code updates

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-  <h1>Welcome to Pulse!</h1>
+  <RouterView></RouterView>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue'),
+    },
+    {
+      // /projects/id
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'NotFound',
+      component: h('p', { style: 'color: red;' }, '404 Not Found'),
+    },
+  ],
 })
 
 export default router
