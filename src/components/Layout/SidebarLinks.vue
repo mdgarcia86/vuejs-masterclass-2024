d34c47ad968c7b0cb3a9f5b6e7d2ebc0933e6fd0
Manuel Garcia <mdgarcia.calpoly@gmail.com>
2025-01-07 14:15:05 -0800

    Finished Chapter 7

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -1,19 +1,39 @@
 <script setup lang="ts">
-
 interface LinkProp {
-  title: string,
-  to: string,
-  icon: string,
+  title: string
+  to?: string
+  icon: string
 }
-defineProps<{
+
+const props = defineProps<{
   links: LinkProp[]
 }>()
+
+const emits = defineEmits<{
+  actionClicked: [string]
+}>()
+
+const emitActionClicked = (linkTitle: string) => {
+  emits('actionClicked', linkTitle)
+}
 </script>
 
 <template>
-  <RouterLink v-for="link in links" :key="link.title" :to="link.to" exactActiveClass="text-primary bg-muted"
-    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
+  <template v-for="link in links" :key="link.title">
+    <RouterLink v-if="link.to" exactActiveClass="text-primary bg-muted" :to="link.to" class="nav-link">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+
+    <div v-else class="nav-link cursor-pointer" @click="emitActionClicked(link.title)">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+  </template>
 </template>
+
+<style scoped>
+.nav-link {
+  @apply flex items-center justify-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary lg:justify-normal text-muted-foreground;
+}
+</style>
--- supabase/migrations/20241219184005_projects_schema.sql
+++ supabase/migrations/20241219184005_projects_schema.sql
@@ -1,10 +1,10 @@
 drop table if exists projects;
+
 drop type if exists current_status;
+create type current_status as enum ('in-progress', 'completed');
 
-create type current_status as enum ('in-progress','completed');
-
-create table
-  Projects (
+create table 
+  projects (
     id bigint primary key generated always as identity not null,
     created_at timestamptz default now() not null,
     name text not null,
@@ -13,5 +13,3 @@ create table
     status current_status default 'in-progress' not null,
     collaborators text array default array[]::varchar[] not null
   );
-
-/*insert into projects (name, slug) values ('project one', 'project-one');*/
